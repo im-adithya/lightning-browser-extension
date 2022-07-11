52e96d85735521ae6a484bcebafbd3f7c7e22d55
im-adithya <imadithyavardhan@gmail.com>
2022-07-11 17:18:29 +0530

    chore: migrate allowance/get action to ts

--- src/types.ts
+++ src/types.ts
@@ -167,6 +167,11 @@ export interface MessageAllowanceList extends MessageDefault {
   action: "listAllowances";
 }
 
+export interface MessageAllowanceGet extends Omit<MessageDefault, "args"> {
+  args: { host: Allowance["host"] };
+  action: "getAllowance";
+}
+
 interface LNURLChannelServiceResponse {
   uri: string; // Remote node address of form node_key@ip_address:port_number
   callback: string; // a second-level URL which would initiate an OpenChannel message from target LN node
@@ -262,7 +267,7 @@ export type Transaction = {
   value: string;
   preimage: string;
   badges: IBadge[];
-  totalAmount: string;
+  totalAmount: number;
   totalFees: string;
   description: string;
   location: string;
